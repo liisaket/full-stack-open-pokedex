7231482f8543e38b408d489f552a70415169f47a
Liisa-Marie <liisa-m@outlook.com>
2025-09-25 20:26:30 +0300

    another e2e test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -5,3 +5,12 @@ describe('Pokedex', function () {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+describe('Pokemon', function () {
+  it('can visit ivysaur', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.url().should('include', '/pokemon/ivysaur')
+    cy.contains('chlorophyll')
+  })
+})
