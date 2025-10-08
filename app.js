30d559d2301830bec5b94f60e7f18ebb0a48e88e
Liisa-Marie <liisa-m@outlook.com>
2025-10-08 19:28:30 +0300

    fix error

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT + 1, () => {
+app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`) //eslint-disable-line
 })
 
